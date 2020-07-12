8cf595f28bb2cd2047c5d7204169ead4deee9941
Kahlil Whitfield <darkristy2002@gmail.com>
2020-07-11 20:01:21 -0400

    setting styled components

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,18 +1,15 @@
 import App from 'next/app';
-import { ThemeProvider } from 'styled-components';
 
 import { GlobalStyles } from '../styles/GlobalStyles';
-
-const theme = {};
 
 export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
-      <ThemeProvider theme={theme}>
+      <>
         <GlobalStyles />
         <Component {...pageProps} />
-      </ThemeProvider>
+      </>
     );
   }
 }
